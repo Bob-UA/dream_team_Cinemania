9aa7e9e22b7a97d5728b4b2367e7db684cc1ace0
IvashkaVIP <86344094+IvashkaVIP@users.noreply.github.com>
2023-06-10 10:23:37 +0300

    Api (#2)
    * first
    
    * add fetch trending
    
    * add fetch bySearch
    
    * add fetch details
    
    * add all fetch
    
    * change path

---
+++ src/js/home_js/home_main.js
@@ -0,0 +1,13 @@
+
+import { getMoviesBySearch, getMoviesDetails, getMoviesGenres, getMoviesTrending, getMoviesUpcoming, getMoviesVideos } from "../api/ApiService";
+
+
+// //let data = {};
+// //const {data} = await getMoviesTrending('day', 4);
+// //const {data} = await getMoviesUpcoming(5);
+// //const { data } = await getMoviesBySearch('p',5,2023);
+// //const { data } = await getMoviesDetails(2023);
+// const { data } = await getMoviesVideos(2023);
+// // const { data } = await getMoviesGenres();
+
+//  console.log(data);
