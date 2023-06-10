0b2c79b8be4b6d707a3dc5fea1785721835c6bbb
IvashkaVIP <ivashka.co@gmail.com>
2023-06-10 10:03:32 +0300

    change path

--- src/js/home_js/home_main.js
+++ src/js/home_js/home_main.js
@@ -1,13 +1,13 @@
 
-import { getMoviesBySearch, getMoviesDetails, getMoviesGenres, getMoviesTrending, getMoviesUpcoming, getMoviesVideos } from "../api_js/fetch_movies";
+import { getMoviesBySearch, getMoviesDetails, getMoviesGenres, getMoviesTrending, getMoviesUpcoming, getMoviesVideos } from "../api/ApiService";
 
 
-//let data = {};
-//const {data} = await getMoviesTrending('day', 4);
-//const {data} = await getMoviesUpcoming(5);
-//const { data } = await getMoviesBySearch('p',5,2023);
-//const { data } = await getMoviesDetails(2023);
-//const { data } = await getMoviesVideos(2023);
-const { data } = await getMoviesGenres();
+// //let data = {};
+// //const {data} = await getMoviesTrending('day', 4);
+// //const {data} = await getMoviesUpcoming(5);
+// //const { data } = await getMoviesBySearch('p',5,2023);
+// //const { data } = await getMoviesDetails(2023);
+// const { data } = await getMoviesVideos(2023);
+// // const { data } = await getMoviesGenres();
 
-console.log(data);
+//  console.log(data);
