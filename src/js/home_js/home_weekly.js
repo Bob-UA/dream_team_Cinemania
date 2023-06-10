690bae9cbf8dd3f9e8da5daad0f620675305e409
shaman2507 <alabay174@gmail.com>
2023-06-10 19:31:18 +0200

    weekly update

--- src/js/home_js/home_weekly.js
+++ src/js/home_js/home_weekly.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
-const BASIC_URL = 'https://api.themoviedb.org/3/search/movie';
+const BASIC_URL = 'https://api.themoviedb.org/3';
 export const IMG_URL = 'https://image.tmdb.org/t/p/w500/';
-const API_KEY = 'd277c204a2c30c818c2befd49f072a72';
+const API_KEY = '471141c69aa19d434e5adba2f5f93507';
 const ENDPOINT = '/trending/movie/week';
+
+
+        
+const list = document.querySelector('.weekly-js-list');
+// const loadMore = document.querySelector('.js-load-more');
 
  async function filmAPI(name) {
    return await axios.request({
@@ -27,5 +32,35 @@ function serviceMovie(page = 1) {
             return resp.json()
         })
 }
-serviceMovie(6)
-.then(data => console.log(data))
+
+// function weeklyTrends(page = 1) {
+//     return fetch(`${BASIC_URL}${ENDPOINT}?api_key=${API_KEY}&page=${page}&results=${results}`)
+//         .then(resp => {
+//             if (!resp.ok) {
+//                 throw new Error(resp.statusText);
+//             }
+        
+//             return resp.json()
+//         })
+// }
+
+
+serviceMovie()
+  .then(data => {
+    list.insertAdjacentHTML('beforeEnd', createMarkUp(data.results))
+  })
+
+// function serviceMovieAp() {
+//   return serviceMovie.map(serviceMovie => )
+// } 
+
+function createMarkUp(arr) {
+  return arr.map(({ original_title, poster_path, genre_ids, vote_average}) => `<li>
+      <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${original_title}" class="weekly-list-img">
+      <h2 class="weekly-list-title">${original_title}</h2>
+      <h3 class="weekly-list-genre">${genre_ids}</h3>
+      <p class="weekly-list-raiting">${vote_average}</p>
+    </li > `).join('')
+}
+  
+
