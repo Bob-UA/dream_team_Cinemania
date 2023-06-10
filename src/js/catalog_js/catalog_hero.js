da9aa8fbb6a4b970d1b55718dc77c80aa924c3b1
fxxwol <protsivroksolana2004@gmail.com>
2023-06-10 19:11:08 +0300

    showing movie of the day

--- src/js/catalog_js/catalog_hero.js
+++ src/js/catalog_js/catalog_hero.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { defaultHeroStyles } from '../utils';
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMG_URL = 'https://image.tmdb.org/t/p/original/';
@@ -12,52 +13,74 @@ function getRandom(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
-
-const randPage = getRandom(1, 500);
+showRandomMovie()
 
 async function fetchMovies() {
     try {
         const response = await axios.get(`${BASE_URL}${ENDPOINT}`, {
             params: {
                 api_key: `${API_KEY}`,
-                page: `${randPage}`
+                page: 1
             }
         });
         return response.data.results;
     } catch (error) {
-        console.error(error);
+        console.error(error)
     }
 }
 
 async function showRandomMovie() {
-    const results = await fetchMovies();
-    const index = getRandom(0, results.length - 1)
-    createHeroMarkup(results[index]);
-}
-
-showRandomMovie()
-
-const createHeroMarkup = ({ backdrop_path, title, vote_average, overview }) => {
-    const markup = ` <h1 class="title hero-title">${title}</h1>
-    <p class="hero-stars">${vote_average}</p>
-      <p class="text hero-text">
-        ${overview}
-      </p>
-      <ul>
-        <li>
-          <a href=""><button class="btn">Watch trailer</button></a>
-        </li>
-         <li>
-          <a href=""><button class="btn">More details</button></a>
-        </li>
-      </ul>`;
-    textContainer.innerHTML = markup;
-    heroSection.style.backgroundImage = `linear-gradient(
+    try {
+        const results = await fetchMovies();
+        const index = getRandom(0, results.length - 1);
+        if (results[index]) {
+            textContainer.innerHTML = createHeroMarkup(results[index]);
+            heroSection.style.backgroundImage = `linear-gradient(
         86.77deg,
         #111111 5%,
         rgba(17, 17, 17, 0) 60%
       ),
-      url(${IMG_URL}${backdrop_path})`
+      url(${IMG_URL}${results[index].backdrop_path})`
+        }
+        else {
+            textContainer.innerHTML = createDefaultHeroMarkup();
+            const style = document.createElement('style');
+            style.innerHTML = defaultHeroStyles;
+            document.head.appendChild(style);
+        }
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+
+const createHeroMarkup = ({ title, vote_average, overview }) => {
+    return ` <h1 class="title hero-title">${title}</h1>
+    <p class="hero-stars">${vote_average}</p>
+      <p class="text hero-text">
+        ${overview}
+      </p>
+      <ul class="btn-list">
+        <li>
+          <a href=""><button class="btn">Watch trailer</button></a>
+        </li>
+         <li>
+          <a href=""><button class="btn-details">More details</button></a>
+        </li>
+      </ul>`;
 };
 
+const createDefaultHeroMarkup = () => {
+    return `<h1 class="title hero-title">Let’s Make Your Own Cinema</h1>
+      <p class="text hero-text">
+        Is a guide to creating a personalized movie theater experience. You'll
+        need a projector, screen, and speakers. <span class="mobile-hidden"> Decorate your space, choose your
+        films, and stock up on snacks for the full experience.</span>
+      </p>
+      <ul class="btn-list">
+        <li>
+          <a href="../../catalog.html"><button class="btn">Get Started</button></a>
+        </li>
+      </ul>`
+}
 console.log(textContainer)
