5dc98216c55c6f2da2c9ab8d840bdb905c565684
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-06-11 15:40:00 -0700

    changes_in_API

--- src/js/api/ApiService.js
+++ src/js/api/ApiService.js
@@ -1,3 +1,4 @@
+
 // API з фільмами (потрібно зареєструватися щоб отримати api_key):	 https://www.themoviedb.org/
 // Документація	https://developer.themoviedb.org/docs
 
@@ -13,14 +14,28 @@
 // Фільми за ключовим словом ( + за роком)	https://developers.themoviedb.org/3/search/search-movies
 // https://api.themoviedb.org/3/search/movie
 
+
 // Детальна інформація про фільм	https://developers.themoviedb.org/3/movies/get-movie-details
 // https://api.themoviedb.org/3/movie/{movie_id}
+
 
 // Повна інформація про можливий трейлер фільма на YouTube	https://developers.themoviedb.org/3/movies/get-movie-videos
 //  https://api.themoviedb.org/3/movie/{movie_id}/videos
 
 // Перелік жанрів	https://developers.themoviedb.org/3/genres/get-movie-list
 // https://api.themoviedb.org/3/genre/movie/list
+//-----------------------------------------------------------------------------------------------------------------------------
+
+// const resp = await getMoviesTrending('day', 4);            // resp.data.results    масив
+// const resp = await getMoviesUpcoming(5);                   // resp.data.results    масив
+// const resp = await getMoviesBySearch('pirates',5,2023);    // resp.data.results    масив
+
+// const resp = await getMoviesDetails(2023);                  // resp.data      не массив!!!
+// const resp = await getMoviesVideos(2023);                   // resp.results   не массив!!!
+// const resp = await getMoviesGenres();                       // resp.data.genres    масив 
+
+
+ //-----------------------------------------------------------------------------------------------------------------------------
 
 // Ключ API
 // 9d709850c7590845ffb60644b29d6f51
@@ -29,11 +44,40 @@ import axios from 'axios';
 
 const API_KEY = '9d709850c7590845ffb60644b29d6f51';
 const BASE_URL = 'https://api.themoviedb.org/3/';
-const TRENDING_END_POINT = 'trending/movie/'; // end-point  трендовых фільмів за день/тиждень
-const UPCOMING_END_POINT = 'movie/upcoming'; // end-point  новінки
-const SEARCH_END_POINT = 'search/movie'; // end-point  фільми за ключовим словом (+ рік)
-const DETAILS_END_POINT = 'movie/'; // end-point детальна інформація про фільм
-const GENRES_END_POINT = 'genre/movie/list'; // end-point жанрів
+const TRENDING_END_POINT = 'trending/movie/';                     // end-point  трендовых фільмів за день/тиждень
+const UPCOMING_END_POINT = 'movie/upcoming';                     // end-point  новінки
+const SEARCH_END_POINT = 'search/movie';                         // end-point  фільми за ключовим словом (+ рік)
+const DETAILS_END_POINT = 'movie/';                              // end-point детальна інформація про фільм
+const GENRES_END_POINT = 'genre/movie/list';                      // end-point жанрів
+
+let arrayOfGenres = (await getMoviesGenres()).data.genres;
+
+// console.log(arrayOfGenres);
+// console.log('   ');
+// let a = (await getMoviesTrending()).data.results[2].genre_ids;
+// // console.log(a)
+
+// const resp = await getMoviesUpcoming(5); // resp.data.results    масив
+// console.log(resp.data.results);
+
+
+
+// //-----------------------------------------------------------------------запит на ым'я жанрів по їх id
+
+// console.log(a);
+// console.log(getNameOfGenreById(a).join(', '));
+
+export function getNameOfGenreById(array) {
+  let res = [];
+  arrayOfGenres.map((gen) => {
+    array.map(item => {
+      if (gen.id === item) res.push(gen.name)
+    });
+  })
+  
+//  console.log(res);
+  return (res);
+}
 
 //-----------------------------------------------------------------------запит на трендові фільми
 //-----------------------------------------------------------------------timeWindow = 'week'/'day'
@@ -64,7 +108,7 @@ export async function getMoviesUpcoming(page = 1) {
 }
 //-----------------------------------------------------------------------запит на детальну інформацію про фільм
 //-----------------------------------------------------------------------якщо рік не вказувати - за весь доступний період
-export async function getMoviesBySearch(query = '', page = 1, year = '') {
+export async function getMoviesBySearch(query='', page = 1, year='') {
   try {
     return await axios(`${BASE_URL}${SEARCH_END_POINT}?`, {
       params: {
@@ -136,5 +180,8 @@ export function handlerError(error) {
   }
   console.log(error.config);
 
-  //alert('there`s something wrong, please see the messages in the console');
+//alert('there`s something wrong, please see the messages in the console');
 }
+
+
+
