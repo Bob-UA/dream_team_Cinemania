606a415fd0b1cf7893c625d11a11e604dd32ed79
vbegunova <veronikabegunova42@gmail.com>
2023-06-14 14:25:23 -0700

    Update movies_cards.js

--- src/js/catalog_js/components/movies_cards.js
+++ src/js/catalog_js/components/movies_cards.js
@@ -2,7 +2,7 @@ function cutStringLength(str) {
   if (str.length > 20 ) {
     return str.substring(0, 20)+"..."
   };
-  return str
+  return str;
 }
 
 export function markup(poster_path, title, id, genres, year, starRating) {
@@ -19,12 +19,12 @@ export function markup(poster_path, title, id, genres, year, starRating) {
   return `<li class="gallery-movies-item" data-id="${id}">
         <img class="gallery-movies-img" src="https://image.tmdb.org/t/p/original/${poster_path}" alt="${title}" loading="lazy">
         <div class="gallery-movies-overlay js-modal-info" data-id="${id}"></div>
-            <div class="gallery-movies-description">
-                <h3 class="gallery-movies-title">${title}</h3>
-                <div class="gallery-movies-wrap">
-                <p class="gallery-movies-details">${str} | ${year}</p>
-                <img src=".${starRating}" alt="raiting" class="star-rating-card"/>
-                </div>
+        <div class="gallery-movies-description">
+            <h3 class="gallery-movies-title">${title}</h3>
+            <div class="gallery-movies-wrap">
+            <p class="gallery-movies-details">${str} | ${year}</p>
+            <img src=".${starRating}" alt="raiting" class="star-rating-card"/>
             </div>
-        </li>`;
+        </div>
+    </li>`;
 }
