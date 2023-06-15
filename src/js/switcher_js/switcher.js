f6d2115d24aef18cfb14d58ca4be06b80284b33e
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-06-15 00:47:12 -0700

    changed_js_for_team_modal

--- src/js/switcher_js/switcher.js
+++ src/js/switcher_js/switcher.js
@@ -96,19 +96,25 @@ function setBackdropColor(theme) {
 const footerBtn = document.querySelector('.footer-btn');
 const backdropModal = document.querySelector('.backdrop-team-modal');
 const closeBtn = document.querySelector('.close-btn');
-
 function openModal() {
   backdropModal.classList.add('show');
+  document.addEventListener('keydown', closeModalOnEscape);
+  backdropModal.addEventListener('click', closeModalOnClickOutside);
 }
-
 function closeModal() {
   backdropModal.classList.remove('show');
+  document.removeEventListener('keydown', closeModalOnEscape);
+  backdropModal.removeEventListener('click', closeModalOnClickOutside);
+}
+function closeModalOnEscape(event) {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+}
+function closeModalOnClickOutside(event) {
+  if (event.target === backdropModal) {
+    closeModal();
+  }
 }
 footerBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
-
-
-
-
-
-
