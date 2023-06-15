7c2225025ecec6e7ca6c1560fac66c6529e2203b
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-06-14 18:54:00 -0700

    path_test

--- src/js/catalog_js/components/catalog_hero.js
+++ src/js/catalog_js/components/catalog_hero.js
@@ -67,10 +67,10 @@ function createMarkUpForError() {
       But we couldn’t find the trailer.
     </h1>
     <picture class="camera">
-      <source srcset="./images/modal-camera-mobile-1x.jpg 1x, ./images/modal-camera-mobile-2x.jpg 2x" media="(max-width: 767px)" />
-      <source srcset="./images/modal-camera-tablet-1x.jpg 1x, ./images/modal-camera-tablet-2x.jpg 2x" media="(min-width: 768px)" />
-      <source srcset="./images/modal-camera-desktop-1x.jpg 1x, ./images/modal-camera-desktop-2x.jpg 2x" media="(min-width: 1280px)" />
-      <img src="./images/modal-camera-mobile-1x.jpg" alt="camera" />
+      <source srcset="./img/modal-camera-mobile-1x.jpg 1x, ./img/modal-camera-mobile-2x.jpg 2x" media="(max-width: 767px)" />
+      <source srcset="./img/modal-camera-tablet-1x.jpg 1x, ./img/modal-camera-tablet-2x.jpg 2x" media="(min-width: 768px)" />
+      <source srcset="./img/modal-camera-desktop-1x.jpg 1x, ./img/modal-camera-desktop-2x.jpg 2x" media="(min-width: 1280px)" />
+      <img src="./img/modal-camera-mobile-1x.jpg" alt="camera" />
     </picture>
   </div>`;
 }
